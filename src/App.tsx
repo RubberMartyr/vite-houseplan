5e91915621a338606fb0afb843d508f2909c4b01
RubberMartyr <120252113+RubberMartyr@users.noreply.github.com>
2026-02-18 12:21:22 +0100

    Add interactive refactoring plan dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import React from 'react'
-import HouseViewer from './components/HouseViewer'
+import RefactoringPlan from './components/RefactoringPlan'
 
 export default function App() {
-  return (
-    <>
-      <div style={{
-        position: "fixed",
-        top: 10,
-        left: 10,
-        zIndex: 99999,
-        padding: "10px 12px",
-        background: "hotpink",
-        color: "black",
-        fontWeight: 800,
-        borderRadius: 8
-      }}>
-        DEBUG APP ACTIVE {Date.now()}
-      </div>
-
-      <HouseViewer />
-    </>
-  )
+  return <RefactoringPlan />
 }

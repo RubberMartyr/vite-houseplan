c6e9366a1243c66fb0fbe03a7e947e3142337097
RubberMartyr <120252113+RubberMartyr@users.noreply.github.com>
2026-02-20 18:02:01 +0100

    Remove debug overlay from App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,5 @@ import React from 'react'
 import HouseViewer from './components/HouseViewer'
 
 export default function App() {
-  return (
-    <>
-      <div style={{
-        position: "fixed",
-        top: 10,
-        left: 10,
-        zIndex: 99999,
-        padding: "10px 12px",
-        background: "hotpink",
-        color: "black",
-        fontWeight: 800,
-        borderRadius: 8
-      }}>
-        DEBUG APP ACTIVE {Date.now()}
-      </div>
-
-      <HouseViewer />
-    </>
-  )
+  return <HouseViewer />
 }
